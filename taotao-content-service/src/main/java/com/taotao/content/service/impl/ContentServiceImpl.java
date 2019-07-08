8134f53d20dd87d813e28616f86d85e7530fff6b
acerdeepin <acerdeepin@qq.com>
2019-07-08 15:16:12 +0800

    jdis

--- taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -3,8 +3,11 @@ package com.taotao.content.service.impl;
 import java.util.Date;
 import java.util.List;
 
+import com.alibaba.fastjson.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.taotao.content.service.jdis.JedisClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.taotao.common.pojo.TaotaoResult;
@@ -16,7 +19,12 @@ import com.taotao.pojo.TbContent;
 public class ContentServiceImpl implements ContentService {
 
 	@Autowired
+	private JedisClient jedisClient;
+	@Autowired
 	private TbContentMapper mapper;
+
+//	@Value("${CONTENT_KEY}")
+	private String CONTENT_KEY = "CONTENT_KEY";
 	
 	@Override
 	public TaotaoResult saveContent(TbContent content) {
@@ -32,7 +40,28 @@ public class ContentServiceImpl implements ContentService {
 
 	@Override
 	public List<TbContent> getContentListByCatId(Long categoryId) {
-		return mapper.selectList(new QueryWrapper<TbContent>().lambda().eq(TbContent::getCategoryId,categoryId));
+
+		List<TbContent> list ;
+		try{
+			String str  =jedisClient.get(CONTENT_KEY);
+			if (str !=null && str.length() > 0){
+				list = JSON.parseArray(str, TbContent.class);
+				System.err.println("---------jedisClient  cache--------------");
+				return  list;
+			}
+		}catch (Exception e1){
+
+		}
+		list = mapper.selectList(new QueryWrapper<TbContent>().lambda().eq(TbContent::getCategoryId,categoryId));
+		try{
+			if (list != null){
+				jedisClient.set(CONTENT_KEY, JSON.toJSONString(list));
+			}
+		}catch (Exception e1){
+
+		}
+
+		return list;
 	}
 
 }
