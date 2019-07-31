3ac1d91a716a5a0ccc0542f428f8f95b94d06403
ssdeepin <ssdeepin@qq.com>
2019-07-31 21:42:39 +0800

    nginx  test

--- taotao-sso-web/src/main/java/com/taotao/sso/controller/PageController.java
+++ taotao-sso-web/src/main/java/com/taotao/sso/controller/PageController.java
@@ -1,16 +1,23 @@
 package com.taotao.sso.controller;
 
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.logging.impl.Log4JLogger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@Log4j
 public class PageController {
+
+
 
     @RequestMapping("/page/{page}")
     @ResponseBody
     public String  showPage(@PathVariable String page){
+        log.error("page = "+page);
         return page;
     }
 }
